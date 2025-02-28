31c5169d58d5360ce59afec0e2288aa2a6f92359
jamro <jamrobmartin@gmail.com>
2025-02-27 22:39:00 -0500

    Add project files.

---
+++ JME/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace JME
+{
+    public class Class1
+    {
+        public static void PrintTest()
+        {
+            Console.WriteLine("Calling PrintTest() from JME Library");
+        }
+    }
+}
---
+++ JMETestGame/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace JMETestGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            JME.Class1.PrintTest();
+        }
+    }
+}
